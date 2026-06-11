d1dfbc4ebc917d13d8317414f20f5508ed48e6f4
LucasHARosa <l.ucas.henrique.a.r@gmail.com>
2026-06-10 22:40:13 -0300

    feat: set up NativeWind with project color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: [
+    './src/app/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
+  presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        redDark: '#BF3B44',
+        redMid: '#F3BABD',
+        redLight: '#F4E6E7',
+
+        greenDark: '#639339',
+        greenMid: '#CBE4B4',
+        greenLight: '#E5F0DB',
+
+        gray1: '#1B1D1E',
+        gray2: '#333638',
+        gray3: '#5C6265',
+        gray4: '#B9BBBC',
+        gray5: '#DDDEDF',
+        gray6: '#EFF0F0',
+        gray7: '#FAFAFA',
+      },
+    },
   },
   plugins: [],
 };
